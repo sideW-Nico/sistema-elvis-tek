ed3e7aaf98acaf318a266e003defe2f60e84ad61
Leandro <leandrolopezhegui@gmail.com>
2026-07-23 23:40:10 -0300

    fix: cambio de hash

--- baseDeDatos/DML/inserccionUsuarios.sql
+++ baseDeDatos/DML/inserccionUsuarios.sql
@@ -2,16 +2,16 @@
     Espacio donde se deberán colocar todas las insercciones utilizadas en la primer ejecución del programa para el testeo.
 */
 
-INSERT INTO USUARIO (cedula, claveHash) VALUES ('11111111', '$2y$12$Qx.Sy1xPSYPmoNRiqf8gCO8JDSu7J/Z7rbOVJ6rsBCQRHConc4rdS');
+INSERT INTO USUARIO (cedula, claveHash) VALUES ('11111111', '$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS');
 
 /*
-    "clave1234567" ~ "$2y$12$Qx.Sy1xPSYPmoNRiqf8gCO8JDSu7J/Z7rbOVJ6rsBCQRHConc4rdS"
+    "clave1234567" ~ "$2y$12$ki0bVkt8cnZuR4v6aJvhhelaeQc1/4fec2txUcuG1Ybr4cvnhg2sS"
 
     El hash se recuperó con el siguiente script para crear el primer usuario en el sistema
     Nota: En el futuro se deberían cargar por un usuario administrador
 
     <?php
-        $return = password_hash("clave1234567", PASSWORD_DEFAULT);
+        $return = password_hash('clave1234567', PASSWORD_DEFAULT);
         echo($return);
     ?>
 */
